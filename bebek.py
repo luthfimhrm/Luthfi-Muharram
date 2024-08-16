6533e4d758f08259ba139920aa44fa12e2341fa9
luthfimuharram <luthfimuharram@apps.ipb.ac.id>
2024-08-16 15:56:53 +0700

    first commit

---
+++ bebek.py
@@ -0,0 +1,9 @@
+C = int(input("Jumlah cokelat yang dimmiliki oleh Pak Dengklek: "))
+
+bebek = 0
+
+while bebek < C:
+    bebek += 1
+    C -= bebek
+
+print (f"Bebek nomor {bebek + 1} tidak mendapatkan cokelat \n")
